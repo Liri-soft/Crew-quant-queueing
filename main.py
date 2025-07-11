f24a051c2ff189631cc5779b0e627c47d8c08476
Raghav .G <Raghav@DESKTOP-6Q6ACAL.dc.lirisoft.net>
2025-07-11 15:28:17 +0530

    added cors and .gitignore

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from api.api_server import router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # React app origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(router)
